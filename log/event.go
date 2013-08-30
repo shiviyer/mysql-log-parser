ee02710ec4e16a04314cfea451b2dc5256518bac
Daniel Nichter <daniel@percona.com>
2013-08-29 19:52:21 -0700

    Remove Class, Id, and Alias from Event struct.  Add log.NewEvent() so the metric maps are initialized.  Rename MicroMetrics to TimeMetrics and IntMetrics to NumberMetrics.

--- log/event.go
+++ log/event.go
@@ -1,22 +1,28 @@
 package log
 
-import (
-	"time"
-)
-
 type Event struct {
 	Offset int64  // byte offset in log file, start of event
-	Ts time.Time  // if present in log file, often times not
+	Ts string     // if present in log file, often times not
 	Admin bool    // Query is admin command not SQL query
 	Query string  // SQL query or admin command
-	Class string  // fingerprint of Query
-	Id uint32     // CRC32 checksum of Class
-	Alias string  // very short form of Query (distill)
 	User string
 	Host string
 	Db string
-	MicroMetrics map[string]float32  // *_time and *_wait metrics
-	IntMetrics map[string]uint32     // most metrics
-	SizeMetrics map[string]uint64    // bytes and sizes metrics
+	TimeMetrics map[string]float32   // *_time and *_wait metrics
+	NumberMetrics map[string]uint64  // most metrics
 	BoolMetrics map[string]bool      // yes/no metrics
+}
+
+func NewEvent() *Event {
+	event := new(Event)
+	event.TimeMetrics = make(map[string]float32)
+	event.NumberMetrics = make(map[string]uint64)
+	event.BoolMetrics = make(map[string]bool)
+	return event
+}
+
+type EventDescription struct {
+	Class string  // fingerprint of Query
+	Id uint32     // CRC32 checksum of Class
+	Alias string  // very short form of Query (distill)
 }
