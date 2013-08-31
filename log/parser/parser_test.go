bea20db1c3bdf51abb173adca2b417c8f4fec6bc
Daniel Nichter <daniel@percona.com>
2013-08-30 20:57:33 -0700

    Use EventsEqual instead of DeepEquals in log/parser/parser_test.go.

--- log/parser/parser_test.go
+++ log/parser/parser_test.go
@@ -1,84 +1,317 @@
 package parser_test
 
 import (
-	"os"
-	l "log"
-	. "launchpad.net/gocheck"
-	"testing"
 	"github.com/percona/percona-go-mysql/log"
 	"github.com/percona/percona-go-mysql/log/parser"
+	. "github.com/percona/percona-go-mysql/test"
+	. "launchpad.net/gocheck"
+	"testing"
 )
 
 // Hook gocheck into the "go test" runner.
 // http://labix.org/gocheck
 func Test(t *testing.T) { TestingT(t) }
 
-type TestSuite struct {
-}
-var _ = Suite(&TestSuite{})
-
-var sample = os.Getenv("GOPATH") + "/src/github.com/percona/percona-go-mysql/test/logs/"
-
-// Start a mock ws server that sends all client msgs back to us via fromClients.
-func (s *TestSuite) SetUpSuite(t *C) {
-}
-
 /////////////////////////////////////////////////////////////////////////////
-// Test cases
+// Slow log test suite
 // //////////////////////////////////////////////////////////////////////////
 
-/*
- * Test slow log parser
- */
+type SlowLogTestSuite struct {
+	p *parser.SlowLogParser
+}
 
-func (s *TestSuite) TestSlowLogParser(t *C) {
-	file, err := os.Open(sample + "slow001.log")
-	if err != nil {
-		l.Fatal(err)
-	}
-	var got []log.Event
-	p := parser.NewSlowLogParser(file, false)
-	go p.Run()
-	for e := range p.EventChan {
-		got = append(got, *e)
-	}
+var _ = Suite(&SlowLogTestSuite{})
+
+func (s *SlowLogTestSuite) TestSlowLogParser(t *C) {
+	got := ParseSlowLog("slow001.log")
 	expect := []log.Event{
 		{
-			Offset: 0,
-			Ts: "071015 21:43:52",
-			Admin: false,
-			Query: `select sleep(2) from n`,
-			User: "root",
-			Host: "localhost",
-			Db: "test",
+			Ts:     "071015 21:43:52",
+			Admin:  false,
+			Query:  `select sleep(2) from n`,
+			User:   "root",
+			Host:   "localhost",
+			Db:     "test",
 			TimeMetrics: map[string]float32{
 				"Query_time": 2,
-				"Lock_time": 0,
+				"Lock_time":  0,
 			},
 			NumberMetrics: map[string]uint64{
-				"Rows_sent": 1,
+				"Rows_sent":     1,
 				"Rows_examined": 0,
 			},
 			BoolMetrics: map[string]bool{},
 		},
 		{
-			Offset: 0,
-			Ts: "071015 21:45:10",
-			Admin: false,
-			Query: `select sleep(2) from test.n`,
-			User: "root",
-			Host: "localhost",
-			Db: "sakila",
+			Ts:     "071015 21:45:10",
+			Admin:  false,
+			Query:  `select sleep(2) from test.n`,
+			User:   "root",
+			Host:   "localhost",
+			Db:     "sakila",
 			TimeMetrics: map[string]float32{
 				"Query_time": 2,
-				"Lock_time": 0,
+				"Lock_time":  0,
 			},
 			NumberMetrics: map[string]uint64{
-				"Rows_sent": 1,
+				"Rows_sent":     1,
 				"Rows_examined": 0,
 			},
 			BoolMetrics: map[string]bool{},
 		},
 	}
-	t.Check(got, DeepEquals, expect)
+	t.Check(got, EventsEqual, &expect)
+
+	got = ParseSlowLog("slow002.log")
+	expect = []log.Event{
+		{
+			Query:  "BEGIN",
+			Ts:     "071218 11:48:27",
+			Admin:  false,
+			User:   "[SQL_SLAVE]",
+			Host:   "",
+			TimeMetrics: map[string]float32{
+				"Query_time": 0.000012,
+				"Lock_time":  0.000000,
+			},
+			NumberMetrics: map[string]uint64{
+				"Merge_passes":  0,
+				"Thread_id":     10,
+				"Rows_examined": 0,
+				"Rows_sent":     0,
+			},
+			BoolMetrics: map[string]bool{
+				"Filesort":          false,
+				"Full_scan":         false,
+				"Filesort_on_disk":  false,
+				"Full_join":         false,
+				"Tmp_table_on_disk": false,
+				"Tmp_table":         false,
+				"QC_Hit":            false,
+			},
+		},
+		{
+			Db: "db1",
+			Query: `update db2.tuningdetail_21_265507 n
+      inner join db1.gonzo a using(gonzo) 
+      set n.column1 = a.column1, n.word3 = a.word3`,
+			Admin:  false,
+			User:   "[SQL_SLAVE]",
+			Host:   "",
+			TimeMetrics: map[string]float32{
+				"Query_time": 0.726052,
+				"Lock_time":  0.000091,
+			},
+			NumberMetrics: map[string]uint64{
+				"Merge_passes":  0,
+				"Thread_id":     10,
+				"Rows_examined": 62951,
+				"Rows_sent":     0,
+			},
+			BoolMetrics: map[string]bool{
+				"Filesort":          false,
+				"Full_scan":         false,
+				"Filesort_on_disk":  false,
+				"Full_join":         false,
+				"Tmp_table_on_disk": false,
+				"Tmp_table":         false,
+				"QC_Hit":            false,
+			},
+		},
+		{
+			Query: `INSERT INTO db3.vendor11gonzo (makef, bizzle)
+VALUES ('', 'Exact')`,
+			Admin:  false,
+			User:   "[SQL_SLAVE]",
+			Host:   "",
+			TimeMetrics: map[string]float32{
+				"InnoDB_queue_wait":    0.000000,
+				"Lock_time":            0.000077,
+				"InnoDB_rec_lock_wait": 0.000000,
+				"Query_time":           0.000512,
+				"InnoDB_IO_r_wait":     0.000000,
+			},
+			NumberMetrics: map[string]uint64{
+				"InnoDB_IO_r_bytes":     0,
+				"Merge_passes":          0,
+				"InnoDB_pages_distinct": 24,
+				"Rows_sent":             0,
+				"Thread_id":             10,
+				"Rows_examined":         0,
+				"InnoDB_IO_r_ops":       0,
+			},
+			BoolMetrics: map[string]bool{
+				"Filesort":          false,
+				"Full_scan":         false,
+				"Filesort_on_disk":  false,
+				"Full_join":         false,
+				"Tmp_table_on_disk": false,
+				"Tmp_table":         false,
+				"QC_Hit":            false,
+			},
+		},
+		{
+			Query: `UPDATE db4.vab3concept1upload
+SET    vab3concept1id = '91848182522'
+WHERE  vab3concept1upload='6994465'`,
+			Admin:  false,
+			User:   "[SQL_SLAVE]",
+			Host:   "",
+			TimeMetrics: map[string]float32{
+				"Query_time":           0.033384,
+				"InnoDB_IO_r_wait":     0.000000,
+				"InnoDB_queue_wait":    0.000000,
+				"Lock_time":            0.000028,
+				"InnoDB_rec_lock_wait": 0.000000,
+			},
+			NumberMetrics: map[string]uint64{
+				"InnoDB_IO_r_bytes":     0,
+				"Merge_passes":          0,
+				"InnoDB_pages_distinct": 11,
+				"Rows_sent":             0,
+				"Thread_id":             10,
+				"Rows_examined":         0,
+				"InnoDB_IO_r_ops":       0,
+			},
+			BoolMetrics: map[string]bool{
+				"Filesort":          false,
+				"Full_scan":         false,
+				"Filesort_on_disk":  false,
+				"Full_join":         false,
+				"Tmp_table_on_disk": false,
+				"Tmp_table":         false,
+				"QC_Hit":            false,
+			},
+		},
+		{
+			Query: `INSERT INTO db1.conch (word3, vid83)
+VALUES ('211', '18')`,
+			Admin:  false,
+			User:   "[SQL_SLAVE]",
+			Host:   "",
+			TimeMetrics: map[string]float32{
+				"InnoDB_queue_wait":    0.000000,
+				"Query_time":           0.000530,
+				"InnoDB_IO_r_wait":     0.000000,
+				"Lock_time":            0.000027,
+				"InnoDB_rec_lock_wait": 0.000000,
+			},
+			NumberMetrics: map[string]uint64{
+				"InnoDB_IO_r_bytes":     0,
+				"Merge_passes":          0,
+				"InnoDB_pages_distinct": 18,
+				"Rows_sent":             0,
+				"Thread_id":             10,
+				"Rows_examined":         0,
+				"InnoDB_IO_r_ops":       0,
+			},
+			BoolMetrics: map[string]bool{
+				"Filesort":          false,
+				"Full_scan":         false,
+				"Filesort_on_disk":  false,
+				"Full_join":         false,
+				"Tmp_table_on_disk": false,
+				"Tmp_table":         false,
+				"QC_Hit":            false,
+			},
+		},
+		{
+			Query: `UPDATE foo.bar
+SET    biz = '91848182522'`,
+			Admin:  false,
+			User:   "[SQL_SLAVE]",
+			Host:   "",
+			TimeMetrics: map[string]float32{
+				"Lock_time":            0.000027,
+				"InnoDB_rec_lock_wait": 0.000000,
+				"InnoDB_queue_wait":    0.000000,
+				"Query_time":           0.000530,
+				"InnoDB_IO_r_wait":     0.000000,
+			},
+			NumberMetrics: map[string]uint64{
+				"InnoDB_IO_r_bytes":     0,
+				"Merge_passes":          0,
+				"InnoDB_pages_distinct": 18,
+				"Rows_sent":             0,
+				"Thread_id":             10,
+				"Rows_examined":         0,
+				"InnoDB_IO_r_ops":       0,
+			},
+			BoolMetrics: map[string]bool{
+				"Filesort":          false,
+				"Full_scan":         false,
+				"Filesort_on_disk":  false,
+				"Full_join":         false,
+				"Tmp_table_on_disk": false,
+				"Tmp_table":         false,
+				"QC_Hit":            false,
+			},
+		},
+		{
+			Query: `UPDATE bizzle.bat
+SET    boop='bop: 899'
+WHERE  fillze='899'`,
+			Admin:  false,
+			User:   "[SQL_SLAVE]",
+			Host:   "",
+			TimeMetrics: map[string]float32{
+				"Query_time":           0.000530,
+				"InnoDB_IO_r_wait":     0.000000,
+				"InnoDB_queue_wait":    0.000000,
+				"Lock_time":            0.000027,
+				"InnoDB_rec_lock_wait": 0.000000,
+			},
+			NumberMetrics: map[string]uint64{
+				"InnoDB_IO_r_bytes":     0,
+				"Merge_passes":          0,
+				"InnoDB_pages_distinct": 18,
+				"Rows_sent":             0,
+				"Thread_id":             10,
+				"Rows_examined":         0,
+				"InnoDB_IO_r_ops":       0,
+			},
+			BoolMetrics: map[string]bool{
+				"Filesort":          false,
+				"Full_scan":         false,
+				"Filesort_on_disk":  false,
+				"Full_join":         false,
+				"Tmp_table_on_disk": false,
+				"Tmp_table":         false,
+				"QC_Hit":            false,
+			},
+		},
+		{
+			Query: `UPDATE foo.bar
+SET    biz = '91848182522'`,
+			Admin:  false,
+			User:   "[SQL_SLAVE]",
+			Host:   "",
+			TimeMetrics: map[string]float32{
+				"Query_time":           0.000530,
+				"Lock_time":            0.000027,
+				"InnoDB_rec_lock_wait": 0.000000,
+				"InnoDB_queue_wait":    0.000000,
+			},
+			NumberMetrics: map[string]uint64{
+				"InnoDB_IO_r_bytes":     0,
+				"Merge_passes":          0,
+				"InnoDB_pages_distinct": 18,
+				"Rows_sent":             0,
+				"Thread_id":             10,
+				"Rows_examined":         0,
+				"InnoDB_IO_r_ops":       0,
+				"InnoDB_IO_r_wait":      0.000000,
+			},
+			BoolMetrics: map[string]bool{
+				"Filesort":          false,
+				"Full_scan":         false,
+				"Filesort_on_disk":  false,
+				"Full_join":         false,
+				"Tmp_table_on_disk": false,
+				"Tmp_table":         false,
+				"QC_Hit":            false,
+			},
+		},
+	}
+	t.Check(got, EventsEqual, &expect)
 }
