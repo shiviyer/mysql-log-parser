84747d1d9f110953482fcc2c3abe36ef26d3a026
Daniel Nichter <daniel@percona.com>
2013-09-01 17:27:41 -0700

    Remove 4th arg to Check(), make comment instead.

--- log/event_test.go
+++ log/event_test.go
@@ -20,7 +20,6 @@ type QueryClassTestSuite struct {
 
 var _ = Suite(&QueryClassTestSuite{})
 
-// No input, no events.
 func (s *QueryClassTestSuite) TestQueryClass(t *C) {
 	var q string
 	var f string
@@ -33,38 +32,39 @@ func (s *QueryClassTestSuite) TestQueryClass(t *C) {
 		f,
 	)
 
+	// Complex comments
 	q = `UPDATE groups_search SET  charter = '   -------3\'\' XXXXXXXXX.\n    \n    -----------------------------------------------------', show_in_list = 'Y' WHERE group_id='aaaaaaaa'`
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"update groups_search set charter = ?, show_in_list = ? where group_id=?",
-		"Complex comments",
 	)
 
+	// Fingerprints all mysqldump SELECTs together
 	q = "SELECT /*!40001 SQL_NO_CACHE */ * FROM `film`"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"mysqldump",
-		"Fingerprints all mysqldump SELECTs together",
 	)
 
+	// Fingerprints stored procedure calls specially
 	q = "CALL foo(1, 2, 3)"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"call foo",
-		"Fingerprints stored procedure calls specially",
 	)
 
+	// Fingerprints admin commands as themselves
 	q = "administrator command: Init DB"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"administrator command: Init DB",
-		"Fingerprints admin commands as themselves",
 	)
 
+	// Fingerprints mk-table-checksum queries together
 	q = "REPLACE /*foo.bar:3/3*/ INTO checksum.checksum (db, tbl, " +
 	    "chunk, boundaries, this_cnt, this_crc) SELECT 'foo', 'bar', " +
 	    "2 AS chunk_num, '`id` >= 2166633', COUNT(*) AS cnt, " +
@@ -86,221 +86,219 @@ func (s *QueryClassTestSuite) TestQueryClass(t *C) {
 		log.QueryClass(q),
 		Equals,
 		"percona-toolkit",
-		"Fingerprints mk-table-checksum queries together",
 	)
 
+	// Removes identifier from USE
 	q = "use `foo`"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"use ?",
-		"Removes identifier from USE",
 	)
 
+	// Removes one-line comments in fingerprints
 	q = "select \n--bar\n foo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select foo",
-		"Removes one-line comments in fingerprints",
 	)
 
+	// Removes one-line comments in fingerprint without mushing things together
 	q = "select foo--bar\nfoo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select foo foo",
-		"Removes one-line comments in fingerprint without mushing things together",
 	)
 
+	// Removes one-line EOL comments in fingerprints
 	q = "select foo -- bar\n"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select foo ",
-		"Removes one-line EOL comments in fingerprints",
 	)
 
+	// Handles bug from perlmonks thread 728718
 	q = "select null, 5.001, 5001. from foo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select ?, ?, ? from foo",
-		"Handles bug from perlmonks thread 728718",
 	)
 
+	// Handles quoted strings
 	q = "select 'hello', '\nhello\n', \"hello\", '\\'' from foo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select ?, ?, ?, ? from foo",
-		"Handles quoted strings",
 	)
 
-
+	// Handles trailing newline
 	q = "select 'hello'\n"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select ?",
-		"Handles trailing newline",
 	)
 
+	// Does not handle all quoted strings
 	// This is a known deficiency, fixes seem to be expensive though.
 	q = "select '\\\\' from foo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select '\\ from foo",
-		"Does not handle all quoted strings",
 	)
 
+	// Collapses whitespace
 	q = "select   foo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select foo",
-		"Collapses whitespace",
 	)
 
+	// Lowercases, replaces integer
 	q = "SELECT * from foo where a = 5"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select * from foo where a = ?",
-		"Lowercases, replaces integer",
 	)
 
+	// Floats
 	q = "select 0e0, +6e-30, -6.00 from foo where a = 5.5 or b=0.5 or c=.5"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select ?, ?, ? from foo where a = ? or b=? or c=?",
-		"Floats",
 	)
 
+	// Hex/bit
 	q = "select 0x0, x'123', 0b1010, b'10101' from foo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select ?, ?, ?, ? from foo",
-		"Hex/bit",
 	)
 
+	// Collapses whitespace
 	q = " select  * from\nfoo where a = 5"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select * from foo where a = ?",
-		"Collapses whitespace",
 	)
 
+	// IN lists
 	q = "select * from foo where a in (5) and b in (5, 8,9 ,9 , 10)"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select * from foo where a in(?+) and b in(?+)",
-		"IN lists",
 	)
 
+	// Numeric table names
 	q = "select foo_1 from foo_2_3"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select foo_? from foo_?_?",
-		"Numeric table names",
 	)
 
+	// Numeric table name prefixes
 	// 123f00 => ?oo because f "looks like it could be a number".
 	q = "select 123foo from 123foo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select ?oo from ?oo",
-		"Numeric table name prefixes",
 	)
 
-
-	q= "select 123_foo from 123_foo"
+	// Numeric table name prefixes with underscores
+	q = "select 123_foo from 123_foo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select ?_foo from ?_foo",
-		"Numeric table name prefixes with underscores",
 	)
 
+	// A string that needs no changes
 	q = "insert into abtemp.coxed select foo.bar from foo"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"insert into abtemp.coxed select foo.bar from foo",
-		"A string that needs no changes",
 	)
 
+	// VALUES lists
 	q = "insert into foo(a, b, c) values(2, 4, 5)"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"insert into foo(a, b, c) values(?+)",
-		"VALUES lists",
 	)
 
+	// VALUES lists with multiple ()
 	q = "insert into foo(a, b, c) values(2, 4, 5) , (2,4,5)"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"insert into foo(a, b, c) values(?+)",
-		"VALUES lists with multiple ()",
 	)
 
+	// VALUES lists with VALUE()
 	q = "insert into foo(a, b, c) value(2, 4, 5)"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"insert into foo(a, b, c) value(?+)",
-		"VALUES lists with VALUE()",
 	)
 
+	// limit alone
 	q = "select * from foo limit 5"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select * from foo limit ?",
-		"limit alone",
 	)
 
+	// limit with comma-offset
 	q = "select * from foo limit 5, 10"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select * from foo limit ?",
-		"limit with comma-offset",
 	)
 
+	// limit with offset
 	q = "select * from foo limit 5 offset 10"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select * from foo limit ?",
-		"limit with offset",
 	)
 
+	// union fingerprints together
 	q = "select 1 union select 2 union select 4"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select ? /*repeat union*/",
-		"union fingerprints together",
 	)
 
+	// union all fingerprints together
 	q = "select 1 union all select 2 union all select 4"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select ? /*repeat union all*/",
-		"union all fingerprints together",
 	)
 
+	// union all fingerprints together
 	q = `select * from (select 1 union all select 2 union all select 4) as x 
 		  join (select 2 union select 2 union select 3) as y`
 	t.Check(
@@ -308,8 +306,8 @@ func (s *QueryClassTestSuite) TestQueryClass(t *C) {
 		Equals,
 		`select * from (select ? /*repeat union all*/) as x 
 		  join (select ? /*repeat union*/) as y`,
-		"union all fingerprints together",
 	)
+
 	/*
 		# Issue 322: mk-query-digest segfault before report
 		t.Check(
@@ -335,24 +333,25 @@ func (s *QueryClassTestSuite) TestQueryClass(t *C) {
 		   "huge insert with explicit columns before values() (issue 322)",
 		)
 	*/
-	// Issue 1030: Fingerprint can remove ORDER BY ASC
 
+	// Remove ASC from ORDER BY
+	// Issue 1030: Fingerprint can remove ORDER BY ASC
 	q = "select c from t where i=1 order by c asc"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select c from t where i=? order by c",
-		"Remove ASC from ORDER BY",
 	)
 
+	// Remove only ASC from ORDER BY
 	q = "select * from t where i=1 order by a, b ASC, d DESC, e asc"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select * from t where i=? order by a, b, d desc, e",
-		"Remove only ASC from ORDER BY",
 	)
 
+	// Remove ASC from spacey ORDER BY
 	q = `select * from t where i=1      order            by 
 		  a,  b          ASC, d    DESC,    
 								 
@@ -361,30 +360,29 @@ func (s *QueryClassTestSuite) TestQueryClass(t *C) {
 		log.QueryClass(q),
 		Equals,
 		"select * from t where i=? order by a, b, d desc, e",
-		"Remove ASC from spacey ORDER BY",
 	)
 
+	// Fingerprint LOAD DATA INFILE
 	q = "LOAD DATA INFILE '/tmp/foo.txt' INTO db.tbl"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"load data infile ? into db.tbl",
-		"Fingerprint LOAD DATA INFILE",
 	)
 
+	// Fingerprint db.tbl<number>name (preserve number)
 	q = "SELECT * FROM prices.rt_5min where id=1"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select * from prices.rt_5min where id=?",
-		"Fingerprint db.tbl<number>name (preserve number)",
 	)
 
+	// Fingerprint /* -- comment */ SELECT (bug 1174956)
 	q = "/* -- S++ SU ABORTABLE -- spd_user: rspadim */SELECT SQL_SMALL_RESULT SQL_CACHE DISTINCT centro_atividade FROM est_dia WHERE unidade_id=1001 AND item_id=67 AND item_id_red=573"
 	t.Check(
 		log.QueryClass(q),
 		Equals,
 		"select sql_small_result sql_cache distinct centro_atividade from est_dia where unidade_id=? and item_id=? and item_id_red=?",
-		"Fingerprint /* -- comment */ SELECT (bug 1174956)",
 	)
 }
